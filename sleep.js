7768e9a0aafe9e186a9311325aaa63a568c6d883
PrinceW3 <scoobydoo.milligan@gmail.com>
2016-05-15 16:40:13 -0400

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,9 +1,18 @@
-// sleep.js v0.1
+// sleep.js v0.0.2
 // Copyright (c) 2016 Reapyr
 
-function sleep(ms) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > ms) break;
-  }
+var sleep = {
+	ms: function(ms) {
+		var start = new Date().getTime();
+		for (var i = 0; i < 1e7; i++) {
+			if ((new Date().getTime() - start) > ms) break;
+		}
+	},
+	
+	s: function(s) {
+		var start = new Date().getTime();
+		for (var i = 0; i < 1e7; i++) {
+			if ((new Date().getTime() - start) > (s * 1000)) break;
+		}
+	}
 }
